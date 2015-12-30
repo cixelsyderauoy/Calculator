9583c63a6aa7863c196a3a41f0a2a03d17d42189
Zachary Silvia <zachary.silvia.zs@gmail.com>
2015-12-30 04:42:09 -0800

    Working 0-9 button(Input and sound) and Output Screen

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -7,18 +7,75 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    enum Operation: String {
+        case Divide = "/"
+        case Multiply = "*"
+        case Subtract = "-"
+        case Add = "+"
+        case Equal = "="
+        case Empty = ""
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBOutlet weak var outputLbl: UILabel!
+    
+    //Sounds
+    var btnSound: AVAudioPlayer!
+    
+    //Main variables
+    var runningNumber = ""
+    var leftValStr = ""
+    var rightValStr = ""
+    var currentOperation: Operation = Operation.Empty
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let path = NSBundle.mainBundle().pathForResource("btn", ofType: "wav")
+        let soundUrl = NSURL(fileURLWithPath: path!)
+        
+        do {
+            try btnSound = AVAudioPlayer(contentsOfURL: soundUrl)
+            btnSound.prepareToPlay()
+        } catch let err as NSError {
+            print(err.debugDescription)
+        }
+    
     }
+
+    //Actions
+    
+    @IBAction func numberPressed(btn: UIButton!) {
+        btnSound.play()
+        
+        runningNumber += "\(btn.tag)"
+        outputLbl.text = runningNumber
+    }
+
+    @IBAction func onDividePressed(sender: AnyObject) {
+        
+    }
+
+    @IBAction func onMultiplyPressed(sender: AnyObject) {
+        
+    }
+
+    @IBAction func onSubtractPressed(sender: AnyObject) {
+        
+    }
+
+    @IBAction func onAddPressed(sender: AnyObject) {
+        
+    }
+
+    @IBAction func onEqualPressed(sender: AnyObject) {
+        
+    }
+
 
 
 }
